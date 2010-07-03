d59f67569069ea85362476a6806474cf62e78ca4
Eric Fredricksen <e@fredricksen.net>
2010-07-03 15:11:09 -0700

    Hush of a more pleasing template-based roster

--- roster.js
+++ roster.js
@@ -13,20 +13,28 @@ $(document).ready(function () {
       return;
     }
     $.each(games, function (key, c) {
+      roster.append(brag(c));
+
+      var p = $("<p/>");
+      p.appendTo(roster);
+      p.text(JSON.stringify(c));
+      
       var p = $("<dt/>");
       p.appendTo(roster);
       var a = $("<a/>", {href: "main.html#" + c.name})
       a.text(c.name);
       a.appendTo(p);
+
       var rm = $("<button/>");
       rm.text("X");
       rm.css("color", "red");
-      rm.appendTo(p);
+      rm.appendTo(roster);
       rm.click(function () {
         delete games[c.name];
         storeRoster(games);
         load();
       });
+
       p = $("<dd/>");
       p.appendTo(roster);
       p.text("Level " + (c.level || 1) + " " + c.race + " " + c['class']);
@@ -41,10 +49,23 @@ $(document).ready(function () {
   
   $("#test").click(function () {
     var n = GenerateName();
-    games[n] = {level: -10,name:n,race:GenerateName(),"class":GenerateName()};
+    games[n] = {traits: {level: -10},name:n,race:GenerateName(),"class":GenerateName()};
     storeRoster(games);
     load();
   });
+  
+  function brag(sheet) {
+    var tmpl = $("#bumble").html();
+    var brag = tmpl.replace(/\$([a-z.]+)/g, function (str, p1) {
+      var dict = sheet;
+      $.each(p1.split("."), function (i,v) {
+        if (!dict) return true;
+        dict = dict[v];
+      });
+      return dict || '';
+    });
+    return $(brag);
+  }
   
   $("#clear").click(function () {
     storeRoster({});
